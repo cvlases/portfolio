9dac919015653d700e09b3e9830062733e612895
Claire Vlases <clairevlases@gmail.com>
2026-06-10 15:06:19 -0400

    planet and about page touch ups

--- src/data/windowTitles.js
+++ src/data/windowTitles.js
@@ -2,7 +2,7 @@ export const windowTitles = {
   'html/about.html': 'About',
   'html/art1.html': 'Art',
   'html/dino.html': 'Dino Game',
-  'html/earth.html': 'Environment',
+  'html/earth.html': 'Planet',
   'html/links.html': 'Links',
   'html/portfolio.html': 'Portfolio',
   'html/terminal.html': 'My Computer',
