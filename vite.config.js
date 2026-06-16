2e5a5bf48877b2caf5345740515bd41bfb444a58
Claire Vlases <clairevlases@gmail.com>
2026-06-16 18:07:32 -0400

    added resume, stuff for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { cpSync } from 'fs';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'copy-static-dirs',
+      closeBundle() {
+        for (const dir of ['html', 'assets', 'css', 'js']) {
+          cpSync(dir, `dist/${dir}`, { recursive: true });
+        }
+      },
+    },
+  ],
 });
